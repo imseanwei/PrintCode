95fe5bd6086d22f4531e31a54a028c1ddfb30c31
imseanwei <SeanWei@Seans-MacBook-Pro.local>
2015-03-11 18:32:20 -0400

    a

--- SquareCell.py
+++ SquareCell.py
@@ -65,59 +65,9 @@ def pressure_off ():
     g.write('M10000')
 
 
-#file_1 - main_print
+#file- 1packaging
 g = G(
-        outfile = "/Users/SeanWei/Documents/Python/PrintCode/squarecell/squarecell_20mm_3mm_main.txt",
-        header = "/Users/SeanWei/Documents/Python/PrintCode/header_batteryprinter.txt",
-        footer = "/Users/SeanWei/Documents/Python/PrintCode/footer_batteryprinter.txt",
-        aerotech_include = False,
-        direct_write = False,
-        print_lines = False, 
-        )
-
-
-#Code starts
-for i in range(number_y):
-    
-    for i in range(number_x):
-        
-        g.set_home(x=0,y=0,Z=0) #lower left corner, nozzle edge and packaging in contact, absolute zero
-        g.feed(printvel)
-        g.abs_move(Z=layerheight)
-        pressure_set(port, press)
-        pressure_on()
-        
-        for i in range(layers):
-            g.rect(square_edge-2*tipsize_pack_in-tipsize_out,square_edge-2*tipsize_pack_in-tipsize_out)
-            
-            for i in range(fill_line_number-1):    
-                g.move(fill_line_width, fill_line_width)
-                g.rect(square_edge-2*tipsize_pack_in-tipsize_out-2*(i+1)*fill_line_width,square_edge-2*tipsize_pack_in-tipsize_out-2*(i+1)*fill_line_width)
-            
-            g.abs_move(x=0,y=0)
-            
-            if layers != 1:
-                g.move(Z=layerheight)
-        
-        pressure_off()
-        g.dwell(3.0)
-        g.rect(square_edge-2*tipsize_pack_in-tipsize_out,square_edge-2*tipsize_pack_in-tipsize_out) #to level residue
-        
-        g.abs_move(Z=5)
-        
-        if number_x != 1:
-            g.move(x=spacing_x+square_edge)
-            g.abs_move(Z=0)
-    
-    g.abs_move(Z=5)
-    
-    if number_y != 1:
-        g.move(y=spacing_y+square_edge)
-        g.abs_move(Z=0)
-        
-#file_2 - pack_print
-g = G(
-        outfile = "/Users/SeanWei/Documents/Python/PrintCode/squarecell/squarecell_20mm_3mm_pack.txt",
+        outfile = "/Users/SeanWei/Documents/Python/PrintCode/squarecell/squarecell_20mm_3mm_1packaging.txt",
         header = "/Users/SeanWei/Documents/Python/PrintCode/header_batteryprinter.txt",
         footer = "/Users/SeanWei/Documents/Python/PrintCode/footer_batteryprinter.txt",
         aerotech_include = False,
@@ -171,6 +121,57 @@ for i in range(number_y):
         g.abs_move(Z=0)
 
 
+#file - 2cathode
+g = G(
+        outfile = "/Users/SeanWei/Documents/Python/PrintCode/squarecell/squarecell_20mm_3mm_2cathode.txt",
+        header = "/Users/SeanWei/Documents/Python/PrintCode/header_batteryprinter.txt",
+        footer = "/Users/SeanWei/Documents/Python/PrintCode/footer_batteryprinter.txt",
+        aerotech_include = False,
+        direct_write = False,
+        print_lines = False, 
+        )
+
+
+#Code starts
+for i in range(number_y):
+    
+    for i in range(number_x):
+        
+        g.set_home(x=0,y=0,Z=0) #lower left corner, nozzle edge and packaging in contact, absolute zero
+        g.feed(printvel)
+        g.abs_move(Z=layerheight)
+        pressure_set(port, press)
+        pressure_on()
+        
+        for i in range(layers):
+            g.rect(square_edge-2*tipsize_pack_in-tipsize_out,square_edge-2*tipsize_pack_in-tipsize_out)
+            
+            for i in range(fill_line_number-1):    
+                g.move(fill_line_width, fill_line_width)
+                g.rect(square_edge-2*tipsize_pack_in-tipsize_out-2*(i+1)*fill_line_width,square_edge-2*tipsize_pack_in-tipsize_out-2*(i+1)*fill_line_width)
+            
+            g.abs_move(x=0,y=0)
+            
+            if layers != 1:
+                g.move(Z=layerheight)
+        
+        pressure_off()
+        g.dwell(3.0)
+        g.rect(square_edge-2*tipsize_pack_in-tipsize_out,square_edge-2*tipsize_pack_in-tipsize_out) #to level residue
+        
+        g.abs_move(Z=5)
+        
+        if number_x != 1:
+            g.move(x=spacing_x+square_edge)
+            g.abs_move(Z=0)
+    
+    g.abs_move(Z=5)
+    
+    if number_y != 1:
+        g.move(y=spacing_y+square_edge)
+        g.abs_move(Z=0)
+        
+        
 #To end
 #g.view(backend='matplotlib')
 #g.view()
